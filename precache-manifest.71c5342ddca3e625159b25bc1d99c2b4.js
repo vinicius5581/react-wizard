c62373f4e89d66091ecd987d92ecdf5619e80cf4
Vinicius Santana <santanaviny@gmail.com>
2018-12-06 12:40:58 -0800

    Updates

---
+++ precache-manifest.71c5342ddca3e625159b25bc1d99c2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbe0a7452339f7ff9cf0",
+    "url": "/react-wizard/static/css/main.bf49f0fc.chunk.css"
+  },
+  {
+    "revision": "cbe0a7452339f7ff9cf0",
+    "url": "/react-wizard/static/js/main.cbe0a745.chunk.js"
+  },
+  {
+    "revision": "c2aeb4b700300de6e7e1",
+    "url": "/react-wizard/static/js/1.c2aeb4b7.chunk.js"
+  },
+  {
+    "revision": "4792f5c95da813f9ebac",
+    "url": "/react-wizard/static/js/runtime~main.4792f5c9.js"
+  },
+  {
+    "revision": "eeaefc56d22e90f6859b5c2eb68e6b77",
+    "url": "/react-wizard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d39ee229403d51fb3ea62bcfe4fca3ce.js"
+  "/react-wizard/precache-manifest.71c5342ddca3e625159b25bc1d99c2b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-wizard/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
